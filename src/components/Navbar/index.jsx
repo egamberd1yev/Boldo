aea76c65def07c074a32b0c04f55f25686b1b5c8
egamberd1yev <jorabekegamberdiyev12@gmail.com>
2025-10-08 15:14:45 +0500

    navbar start

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Container} from "@chakra-ui/react"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Container>
+        <img src="src/assets/Logo.svg" alt="" />
+      </Container>
+    </nav>
+  )
+}
+
+export default Navbar
